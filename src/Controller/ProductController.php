3578b9590541ef2186b2bb856021c79d66945b84
MAITRIYA SRAMANER <maitriya@outlook.fr>
2025-04-27 16:18:54 +0200

    product show

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
+
 
 final class ProductController extends AbstractController
 {
@@ -18,5 +20,23 @@ final class ProductController extends AbstractController
             'produitElegance' => $produitElegance,
         ]);
     
+    }
+
+    #[Route('/sport', name:'app_sport')]
+    public function sport(ProductRepository $productRepository): Response
+    {
+
+        $produitSport = $productRepository->findBy(['category' => '1']);
+        return $this->render('product/sport.html.twig', [
+            'produitSport' => $produitSport,
+        ]);
+    }
+
+    #[Route('/produit{id}', name:'app_show')]
+    public function show(Product $produit): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'produit' => $produit,
+        ]);
     }
 }
